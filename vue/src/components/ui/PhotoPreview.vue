02867fb150fde3213ff888861630ad2b4915d3e1
Danila <danila.dvoretskov@yandex.ru>
2024-02-17 13:07:18 +0300

    first_commit

---
+++ vue/src/components/ui/PhotoPreview.vue
@@ -0,0 +1,39 @@
+<template>
+    <img :class=size :src=photo  alt="">
+</template>
+
+<script>
+    export default{
+        name: 'PhotoPreview',
+        props: {
+            photo: {
+                default: null,
+                type: String
+            },
+            size: {
+                default: 'middle',
+                type: String
+            }
+    }
+    }
+</script>
+
+<style scoped lang="less">
+    .large{
+        width: 600px;
+        height: 450px;
+        object-fit: contain;
+    }
+
+    .middle{
+        width: 300px;
+        height: 300px;
+        object-fit: contain;
+    }
+
+    .small{
+        width: 150px;
+        height: 150px;
+        object-fit: contain;
+    }
+</style>
