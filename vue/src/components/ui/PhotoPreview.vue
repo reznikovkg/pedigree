c0eea583cb70db33f58797b540e316f24e129126
Danila <danila.dvoretskov@yandex.ru>
2024-02-19 13:18:50 +0300

    third_commit

--- vue/src/components/ui/PhotoPreview.vue
+++ vue/src/components/ui/PhotoPreview.vue
@@ -1,44 +1,42 @@
 <template>
   <div v-if="photo">
-    <img :class="'img__' + size" :src="photo"  alt="">
+    <img id="photo_preview" :class="'img--' + size" :src="photo"  alt="">
   </div>
 </template>
 
 <script>
-    export default{
-        name: 'PhotoPreview',
-        props: {
-            photo: {
-                default: null,
-                type: String
-            },
-            size: {
-                default: 'middle',
-                type: String
-            }
-        }
-    }
+  export default{
+    name: 'PhotoPreview',
+    props: {
+      photo: {
+        default: null,
+        type: String
+      },
+      size: {
+        default: 'middle',
+        type: String
+      }
+   }
+}
 </script>
 
 <style scoped lang="less">
-    .img__large{
-        width: 200px;
-        height: 300px;
-        object-fit: cover;
-        border-radius: 12px;
-    }
+  #photo_preview{
+    object-fit: cover;
+    border-radius: 12px;
+  }
+  .img--large{
+    width: 200px;
+    height: 300px;
+  }
 
-    .img__middle{
-        width: 150px;
-        height: 200px;
-        object-fit: cover;
-        border-radius: 12px;
-    }
+  .img--middle{
+    width: 150px;
+    height: 200px;
+}
 
-    .img__small{
-        width: 80px;
-        height: 120px;
-        object-fit: cover;
-        border-radius: 12px;
-    }
+  .img--small{
+    width: 80px;
+    height: 120px;
+  }
 </style>
