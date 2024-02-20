7c24e354e6122dde8a330c31b0e2c21eb09df3c5
Alexandr Plotnikov <admiralbarzhi@gmail.com>
2024-02-20 21:27:45 +0300

    commit_1

---
+++ vue/src/components/forms/WeddingForms.vue
@@ -0,0 +1,53 @@
+<template>
+  <form class="wedding_form">
+    <input class="wedding_form--person" type="text" placeholder="Партнёр" v-model="person">
+    <input class="wedding_form--date_start" type="text" placeholder="Дата свадьбы" v-model="date_start">
+    <input class="wedding_form--date_end" type="text" placeholder="Дата развода" v-model="date_end">
+  </form>
+</template>
+
+<script>
+export default{
+  name: 'WeddingForm',
+  props: {
+    wedding: {
+      default: null,
+      type: Object
+    }
+  },
+  data: (props) => {
+    return{
+      person: props.wedding.partner.name,
+      date_start: props.wedding.date_start,
+      date_end: props.wedding.date_end,
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.wedding_form{
+  width: 450px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+  & input{
+    background: #f9f9f9;
+    border-radius: 4px;
+    padding: 5px;
+    border: 1px solid #acacac;
+  }
+  &--person{
+    grid-area: 1 / 1 / 2 / 2;
+  }
+  &--date_start{
+    grid-area: 2 / 1 / 3 / 2;
+  }
+  &--date_end{
+    grid-area: 2 / 2 / 3 / 2;
+  }
+}
+
+</style>
--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -4,18 +4,34 @@
       Homepage
       <button @click="() => openHelpModal()">Open modal</button>
     </section>
+    <WeddingForm :wedding="wedding" />
   </PageLayout>
 </template>
 
 <script>
 import PageLayout from '../parts/PageLayout'
 import { helpModal } from "@/mixins/modals";
+import WeddingForm from '../forms/WeddingForm.vue';
 
 export default {
   name: 'HomePage',
   mixins: [helpModal],
+  data: () => {
+    return{
+      wedding: {
+        partner: {
+          id: '1',
+          name: 'Ivan',
+          second_name: 'Ivanov'
+        },
+        date_start: '01.01.2024',
+        date_end: '01.02.2024'
+      },
+    }
+  },
   components: {
-    PageLayout
+    PageLayout,
+    WeddingForm,
   }
 }
 </script>
