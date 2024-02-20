9e581ac45a44c68ff060e2da5e2ae11b96c05181
егор боклагов <boklagov.egor@yandex.ru>
2024-02-20 23:26:41 +0300

    third_commit

--- vue/src/components/forms/EducationForm.vue
+++ vue/src/components/forms/EducationForm.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="education-form">
     <form @submit.prevent="submitForm">
-        <div class="form-row">
-          <div class="form-group">
-            <input class="form-group--Educ-inp" type="text"  v-model="formData.type" placeholder="Тип">
-          </div>
-          <div class="form-group">
-            <input class="form-group--Educ-inp" type="text"  v-model="formData.level" placeholder="Уровень">
-          </div>
+      <div class="education-form__form-row">
+        <div class="education-form__form-row__form-group">
+          <input class="education-form__form-row__form-group__educ-inp" type="text"  v-model="formData.type" placeholder="Тип">
         </div>
-        <div class="form-row">
-          <div class="form-group">
-            <input class="form-group--Educ-inp" type="text"  v-model="formData.startDate" placeholder="Дата начала обучения">
-          </div>
-          <div class="form-group">
-            <input class="form-group--Educ-inp" type="text" v-model="formData.endDate" placeholder="Дата завершения обучения">
-          </div>
+        <div class="education-form__form-row__form-group">
+          <input class="education-form__form-row__form-group__educ-inp" type="text"  v-model="formData.level" placeholder="Уровень">
         </div>
-        <div class="form-row">
-          <div class="form-group">
-            <input class="form-group--Educ-inp" type="text" v-model="formData.institutionName" placeholder="Название учреждения">
-          </div>
-          <div class="form-group">
-            <input class="form-group--Educ-inp" type="text" v-model="formData.institutionCity" placeholder="Город учреждения">
-          </div>
+      </div>
+      <div class="education-form__form-row">
+        <div class="education-form__form-row__form-group">
+          <input class="education-form__form-row__form-group__educ-inp" type="text"  v-model="formData.startDate" placeholder="Дата начала обучения">
         </div>
+        <div class="education-form__form-row__form-group">
+          <input class="education-form__form-row__form-group__educ-inp" type="text" v-model="formData.endDate" placeholder="Дата завершения обучения">
+        </div>
+      </div>
+      <div class="education-form__form-row">
+        <div class="education-form__form-row__form-group">
+          <input class="education-form__form-row__form-group__educ-inp" type="text" v-model="formData.institutionName" placeholder="Название учреждения">
+        </div>
+        <div class="education-form__form-row__form-group">
+          <input class="education-form__form-row__form-group__educ-inp" type="text" v-model="formData.institutionCity" placeholder="Город учреждения">
+        </div>
+      </div>
     </form>
   </div>
 </template>
@@ -55,7 +55,7 @@ export default {
   mounted() {
     this.formData = { ...this.education };
   },
-  
+
   methods: {
     submitForm() {
       this.$emit('submit', this.formData);
@@ -66,35 +66,31 @@ export default {
 
  <style scoped lang = "less">
 .education-form {
-  max-width: 450px;
+  max-width: 400px;
   height:150px;
   background-color:white;
-}
 
-.form-row {
+
+&__form-row {
   display: flex;
   justify-content: space-between;
-  margin-right:30px;
-  margin-left:30px;
-}
+  margin-left:11px;
 
-.form-group {
+
+&__form-group {
   width: 195px;
   height: 40px;
   margin-top:8px;
 
-&--Educ-inp {
+&__educ-inp {
   padding: 0.5rem;
   border: 1px solid #ccc;
   border-radius: 5px;
   border-color: rgba(0,0,0,0.6);
   background-color: rgba(0,0,0,0.04);
   height: 50%;
-}
-
-&--Educ-inp::placeholder {
-  color: rgba(0,0,0,0.35);
-  font-weight: bold;
+      }
     }
+  }
 }
 </style>
