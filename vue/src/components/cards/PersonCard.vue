9d7809209b7708eec42de76293f7303601099ea4
user <user@UbuntaWeb.myguest.virtualbox.org>
2024-02-20 18:59:11 +0300

    init PersonCard

---
+++ vue/src/components/cards/PersonCard.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="person-card">
+      <br>
+      <div class="person-card__name">{{ person.secondName }} {{ person.firstName }} {{ person.patronymic }}</div>
+      <div class="person-card__dates"> {{ formattedDates }}</div>
+      <br>
+      <div class="person-card__label-names">Родители<br><span class="person-card__information-text">-</span></div>
+      <br>
+      <div class="person-card__label-names">Дети<br><span class="person-card__information-text">-</span></div>
+      <br>
+      <div class="person-card__label-names">Род деятельности<br><span class="person-card__information-text">{{ person.activity }}</span></div>
+      <br>
+      <div class="person-card__label-names"><strong>Биография</strong><br><span class="person-card__information-text">{{ person.biography }}</span></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      person: {
+        type: Object,
+        required: true
+      }
+    },
+    computed: {
+      formattedDates() {
+        if (this.person.die_date) {
+          return `${this.person.birth_date} - ${this.person.die_date}`;
+        } else {
+          return `${this.person.birth_date}`;
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style scoped lang ="less">
+  .person-card {
+    font-family: Arial;
+    font-size: 14px;
+      
+    &__name {
+      font-size: 18px;
+      font-weight: bold;
+    }
+
+    &__dates {
+      font-size: 12px;
+      color: grey;
+    }
+    &__label-names {
+      font-weight: bold;
+    }
+    &__information-text {
+      font-weight: normal;
+      font-size: 10px;
+    }      
+  }
+  </style>
+  
--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -3,6 +3,7 @@
     <section class="p-16">
       Homepage
       <button @click="() => openHelpModal()">Open modal</button>
+      <PersonCard :person="person" />
     </section>
   </PageLayout>
 </template>
@@ -10,12 +11,33 @@
 <script>
 import PageLayout from '../parts/PageLayout'
 import { helpModal } from "@/mixins/modals";
+import PersonCard from '@/components/cards/PersonCard.vue';
+
+
 
 export default {
   name: 'HomePage',
   mixins: [helpModal],
   components: {
-    PageLayout
+    PageLayout,
+    PersonCard
+  },
+  data() {
+    return {
+      person: {
+        id: 'gi6437tf63i4ftfo43f',
+        secondName: 'Иванов',
+        firstName: 'Иван',
+        patronymic: 'Иванович',
+        birth_date: '01.01.2000',
+        die_date: '',
+        gender: 'male',
+        children: ['y3g46yf34fy636b','3345fg35yg34f3f46'],
+        biography: 'Родился в ...',
+        activity: 'Бухгалтер',
+        photo: '/pictures/ivan.jpg'
+      }
+    };
   }
 }
 </script>
