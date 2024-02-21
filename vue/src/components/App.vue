73ee2ba39aee77eb9bf41e0fce6e7cb7281d3754
doldarja1 <doldarja@gmail.com>
2024-02-21 18:24:44 +0300

    v procese

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <RouterView />
+    <ButtonComponent />
     <ModalContainer />
+    
   </div>
 </template>
 
 <script>
+import ButtonComponent from "@/components/ButtonComponent";
 import ModalContainer from "@/components/parts/ModalContainer";
 
 export default {
   components: {
+    ButtonComponent,
     ModalContainer
   }
 }
---
+++ vue/src/components/ButtonComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-button type="primary">App</el-button>
+  </template>
+  
+  <script>
+  import { ElButton } from 'element-ui';
+  
+  export default {
+    components: {
+      ElButton
+    }
+  }
+  </script>
+  
+  
