dad297a3fbda7fde4d4cf39a607ac111924c3cfa
user <user@UbuntaWeb.myguest.virtualbox.org>
2024-02-21 17:17:52 +0300

    xorrected errors and indentations

--- vue/src/components/cards/PersonCard.vue
+++ vue/src/components/cards/PersonCard.vue
@@ -1,60 +1,61 @@
 <template>
-    <div class="person-card">
-      <br>
-      <div class="person-card__name">{{ person.secondName }} {{ person.firstName }} {{ person.patronymic }}</div>
-      <div class="person-card__dates"> {{ formattedDates }}</div>
-      <br>
-      <div class="person-card__label-names">Родители<br><span class="person-card__information-text">-</span></div>
-      <br>
-      <div class="person-card__label-names">Дети<br><span class="person-card__information-text">-</span></div>
-      <br>
-      <div class="person-card__label-names">Род деятельности<br><span class="person-card__information-text">{{ person.activity }}</span></div>
-      <br>
-      <div class="person-card__label-names"><strong>Биография</strong><br><span class="person-card__information-text">{{ person.biography }}</span></div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      person: {
-        type: Object,
-        required: true
-      }
-    },
-    computed: {
-      formattedDates() {
-        if (this.person.die_date) {
-          return `${this.person.birth_date} - ${this.person.die_date}`;
-        } else {
-          return `${this.person.birth_date}`;
-        }
-      }
-    }
-  }
-  </script>
-  
-  <style scoped lang ="less">
-  .person-card {
-    font-family: Arial;
-    font-size: 14px;
-      
-    &__name {
-      font-size: 18px;
-      font-weight: bold;
-    }
+  <div class="person-card">
+    <h1 class="person-card__name">{{ fullName }}</h1>
+    <h2 v-if="person.die_date" class="person-card__dates">{{ `${person.birth_date} - ${person.die_date}` }}</h2>
+    <h2 v-else class="person-card__dates">{{ person.birth_date }}</h2>
+    <h1 class="person-card__label-names">Родители<h2 class="person-card__information-text">-</h2> </h1>
+    <h1 class="person-card__label-names">Дети<h2 class="person-card__information-text">-</h2></h1>
+    <h1 class="person-card__label-names">Род деятельности<h2 class="person-card__information-text">{{ person.activity }}</h2></h1>
+    <h1 class="person-card__label-names">Биография<h2 class="person-card__information-text">{{ person.biography }}</h2></h1>
+  </div>
+</template>
 
-    &__dates {
-      font-size: 12px;
-      color: grey;
+<script>
+export default {
+  props: {
+    person: {
+      type: Object,
+      required: true
     }
-    &__label-names {
-      font-weight: bold;
+  },
+  computed: {
+    fullName() {
+      return `${this.person.secondName} ${this.person.firstName} ${this.person.patronymic}`;
     }
-    &__information-text {
-      font-weight: normal;
-      font-size: 10px;
-    }      
   }
-  </style>
+}
+</script>
+
+
+<style scoped lang ="less">
+.person-card {
   
+  &__name {
+    font-size: 18px;
+    font-weight: 700;
+    font-family:Arial;
+    color:black;
+  }
+  &__dates {
+    font-size: 12px;
+    color: grey;
+    font-family:Arial;
+    padding-bottom:20px;
+  }
+  &__label-names {
+    font-size: 14px;
+    font-weight: 700;
+    font-family:Arial;
+    color:black;
+  }
+  &__information-text {
+    font-size: 14px;
+    font-weight: normal;
+    font-size: 10px;
+    padding-bottom:20px;
+    font-family:Arial;
+    color:black;
+  } 
+     
+}
+</style>
