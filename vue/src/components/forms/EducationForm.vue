16cd7efbad1b198fed3e04b0b367ba9e9b447740
егор боклагов <boklagov.egor@yandex.ru>
2024-02-21 19:51:06 +0300

    five_commit

--- vue/src/components/forms/EducationForm.vue
+++ vue/src/components/forms/EducationForm.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="education-form">
     <form @submit.prevent="submitForm">
-      <div class="education-form__form-row">
-        <div class="education-form__form-row__form-group">
-          <input class="education-form__form-row__form-group__educ-inp" type="text"  v-model="formData.type" placeholder="Тип">
+      <div class="education-form__form-grid">
+        <div class="education-form__form-grid__form-group">
+          <input class="education-form__form-grid__form-group__educ-inp" type="text" v-model="formData.type" placeholder="Тип">
         </div>
-        <div class="education-form__form-row__form-group">
-          <input class="education-form__form-row__form-group__educ-inp" type="text"  v-model="formData.level" placeholder="Уровень">
+        <div class="education-form__form-grid__form-group">
+          <input class="education-form__form-grid__form-group__educ-inp" type="text" v-model="formData.level" placeholder="Уровень">
         </div>
-      </div>
-      <div class="education-form__form-row">
-        <div class="education-form__form-row__form-group">
-          <input class="education-form__form-row__form-group__educ-inp" type="text"  v-model="formData.startDate" placeholder="Дата начала обучения">
+        <div class="education-form__form-grid__form-group">
+          <input class="education-form__form-grid__form-group__educ-inp" type="text" v-model="formData.startDate" placeholder="Дата начала обучения">
         </div>
-        <div class="education-form__form-row__form-group">
-          <input class="education-form__form-row__form-group__educ-inp" type="text" v-model="formData.endDate" placeholder="Дата завершения обучения">
+        <div class="education-form__form-grid__form-group">
+          <input class="education-form__form-grid__form-group__educ-inp" type="text" v-model="formData.endDate" placeholder="Дата завершения обучения">
         </div>
-      </div>
-      <div class="education-form__form-row">
-        <div class="education-form__form-row__form-group">
-          <input class="education-form__form-row__form-group__educ-inp" type="text" v-model="formData.institutionName" placeholder="Название учреждения">
+        <div class="education-form__form-grid__form-group">
+          <input class="education-form__form-grid__form-group__educ-inp" type="text" v-model="formData.institutionName" placeholder="Название учреждения">
         </div>
-        <div class="education-form__form-row__form-group">
-          <input class="education-form__form-row__form-group__educ-inp" type="text" v-model="formData.institutionCity" placeholder="Город учреждения">
+        <div class="education-form__form-grid__form-group">
+          <input class="education-form__form-grid__form-group__educ-inp" type="text" v-model="formData.institutionCity" placeholder="Город учреждения">
         </div>
       </div>
     </form>
@@ -38,7 +34,6 @@ export default {
       required: true
     }
   },
-
   data() {
     return {
       formData: {
@@ -51,11 +46,9 @@ export default {
       }
     }
   },
-
-  mounted() {
+  mounted () {
     this.formData = { ...this.education };
   },
-
   methods: {
     submitForm() {
       this.$emit('submit', this.formData);
@@ -69,21 +62,17 @@ export default {
   max-width: 400px;
   height: 150px;
   background-color: white;
-
-
-  &__form-row {
-    display: flex;
-    justify-content: space-between;
-    margin-left: 11px;
-  
-  
+  &__form-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 5px;
+    margin-left: 10px;
     &__form-group {
       width: 195px;
-      height: 40px;
-      margin-top: 8px;
-  
+      height: 35px;
+      margin-top: 9px;
       &__educ-inp {
-        padding: 0.5rem;
+        padding: 8px;
         border: 1px solid #ccc;
         border-radius: 5px;
         border-color: rgba(0,0,0,0.6);
