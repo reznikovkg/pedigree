ea8ee950d797abf9d27162f6ceb1010f5e8247c7
Santiago-nve <Santiagonvebibang3@gmail.com>
2024-02-23 17:21:34 +0300

    Solution

---
+++ vue/src/components/ui/NavigationPanel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navigation-panel">
+    <SimpleButton type="info">Настройки</SimpleButton>
+    <SimpleButton type="warning">В центр</SimpleButton>
+    <SimpleButton type="danger">Удалить</SimpleButton>
+    <SimpleButton type="primary">Добавить</SimpleButton>
+  </div>
+</template>
+
+<script>
+import SimpleButton from "./SimpleButton.vue";
+
+export default {
+  components: {
+    SimpleButton,
+  },
+};
+</script>
+
+<style scoped>
+.navigation-panel {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.navigation-panel .simple-button {
+  flex: 1;
+  width: 150px;
+  margin-right: 10px;
+}
+
+.navigation-panel .simple-button:last-child {
+  margin-right: 0;
+}
+</style>
---
+++ vue/src/components/ui/SimpleButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="['simple-button', `button-${type}`]">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+      validator: function (value) {
+        return ["info", "warning", "danger", "primary"].includes(value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.simple-button {
+  padding: 15px 20px; 
+  font-size: 16px; 
+  border: none;
+  border-radius: 8px; 
+  cursor: pointer;
+
+  
+  font-family: "Arial", sans-serif; 
+}
+
+.button-info {
+  background-color: #ccc; 
+  color: #000;
+}
+
+.button-warning {
+  background-color: #ff0; 
+  color: #000;
+}
+
+.button-danger {
+  background-color: #f00; 
+  color: #fff;
+}
+
+.button-primary {
+  background-color: #5bc0de; 
+  color: #fff;
+}
+</style>
