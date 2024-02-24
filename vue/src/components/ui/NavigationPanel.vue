891b79b4ae627f3408b6a6504e2148f8bc116ac1
Santiago-nve <Santiagonvebibang3@gmail.com>
2024-02-24 11:31:18 +0300

    Solution

--- vue/src/components/ui/NavigationPanel.vue
+++ vue/src/components/ui/NavigationPanel.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="navigation-panel">
-    <SimpleButton type="info">Настройки</SimpleButton>
-    <SimpleButton type="warning">В центр</SimpleButton>
-    <SimpleButton type="danger">Удалить</SimpleButton>
-    <SimpleButton type="primary">Добавить</SimpleButton>
+    <SimpleButton type="info">Button 1</SimpleButton>
+    <SimpleButton type="warning">Button 2</SimpleButton>
+    <SimpleButton type="danger">Button 3</SimpleButton>
+    <SimpleButton type="primary">Button 4</SimpleButton>
   </div>
 </template>
 
 <script>
-import SimpleButton from "./SimpleButton.vue";
+import SimpleButton from "@/components/ui/SimpleButton";
 
 export default {
   components: {
-    SimpleButton,
-  },
+    SimpleButton
+  }
 };
 </script>
 
@@ -22,15 +22,5 @@ export default {
   display: flex;
   justify-content: space-between;
   padding: 10px;
-}
-
-.navigation-panel .simple-button {
-  flex: 1;
-  width: 150px;
-  margin-right: 10px;
-}
-
-.navigation-panel .simple-button:last-child {
-  margin-right: 0;
 }
 </style>
