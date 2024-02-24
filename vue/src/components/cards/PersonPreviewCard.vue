29ad8e07948e2b4723813abdbaf7be013be25bf7
MRV <sabudilovskiy@yandex.ru>
2024-02-24 20:35:27 +0000

    prefix

--- vue/src/components/cards/PersonPreviewCard.vue
+++ vue/src/components/cards/PersonPreviewCard.vue
@@ -1,86 +1,78 @@
 <template>
 <div class="person-preview-card">
-    <div class="person-info">
-        <h2 class="second-name">{{ person.secondName }}</h2>
-        <h2 class="first-name">{{ person.firstName }}</h2>
-        <h2 class="patronymic">{{ person.patronymic }}</h2>
-        <span class="birth-date" style="display: block;">{{ person.birthDate }}<span v-if="person.die_date"> [- {{ person.die_date}}]</span></span>
-        <span class="person-id" style="display: block;">id: {{ person.id }}</span>
-    </div>
-    <div class="status-indicator"></div>
+  <div class="person-info">
+   <h2 class="second-name">{{ person.secondName }}</h2>
+   <h2 class="first-name">{{ person.firstName }}</h2>
+   <h2 class="patronymic">{{ person.patronymic }}</h2>
+   <span class="birth-date">{{ person.birthDate }}</span>
+   <span v-if="person.die_date" class="birth-date">- {{ person.die_date}}</span>
+   <span class="person-id" style="display: block;">id: {{ person.id }}</span>
+  </div>
+  <div class="status-indicator"></div>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'PersonPreviewCard',
-    props: {
-        person: {
-            type: Object,
-            required: true,
-        },
-    },
+  name: 'PersonPreviewCard',
+  props: {
+   person: {
+    type: Object,
+    required: true,
+   },
+  },
 };
 </script>
 
 <style scoped>
 .person-preview-card {
-    background-color: white;
-    border-radius: 24px;
-    background-color: #f5f5f5;
-    box-shadow: 1.5px 7px 5px 1.5px rgba(0, 0, 0, 0.1), -1.5px 7px 5px -1.5px rgba(0, 0, 0, 0.2);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1em;
-    position: relative;
-    margin: 1em;
-    width: 425px;
-    height: 215px;
-}
-
-.person-info {
-
-    .second-name,
-    .first-name,
-    .patronymic,
-    .birth-date,
-    .person-id {
-        margin-left: 5%;
-        font-family: 'Inter', sans-serif;
-    }
-}
-
-.second-name {
+  background-color: white;
+  border-radius: 24px;
+  background-color: #f5f5f5;
+  box-shadow: 1.5px 7px 5px 1.5px rgba(0, 0, 0, 0.1), -1.5px 7px 5px -1.5px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+  position: relative;
+  margin: 1em;
+  width: 425px;
+  height: 215px;
+  .person-info {
+   .second-name,
+   .first-name,
+   .patronymic,
+   .birth-date,
+   .person-id {
+     margin-left: 5%;
+     font-family: 'Inter', sans-serif;
+   }
+   .second-name {
     margin-top: 5%;
     margin-bottom: 5%;
     color: black;
-}
-
-.first-name {
-    margin-bottom: 5%;
-    color: black;
-}
-
-.patronymic {
-    margin-bottom: 5%;
-    color: black;
-}
-
-.birth-date {
-    margin-bottom: 17%;
+   }
+   .first-name {
+     margin-bottom: 5%;
+     color: black;
+   }
+   .patronymic {
+     margin-bottom: 5%;
+     color: black;
+   }
+   .birth-date {
     color: rgb(117, 117, 117);
     font-weight: bold;
     white-space: nowrap;
-}
-
-.person-id {
-    margin-bottom: 10%;
-    font-weight: bold;
-    color: rgb(117, 117, 117);
-}
-
-.status-indicator {
+    }
+    .person-id {
+       margin-top: 17%;
+       margin-bottom: 10%;
+       font-weight: bold;
+       color: rgb(117, 117, 117);
+    }
+  }
+  .status-indicator {
     width: 25px;
     height: 25px;
     border-radius: 50%;
@@ -90,5 +82,7 @@ export default {
     position: absolute;
     top: 10px;
     right: 10px;
+  }
 }
+
 </style>
