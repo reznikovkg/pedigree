ad34038434f2096195749ef6d0976bfb7aab2a55
егор боклагов <boklagov.egor@yandex.ru>
2024-02-24 03:29:27 +0300

    1_commit

--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -7,6 +7,7 @@
     </section>
     <PhotoPreview size="large" photo="https://mykaleidoscope.ru/x/uploads/posts/2022-10/1666206241_12-mykaleidoscope-ru-p-kartinka-na-zastavku-oboi-12.jpg" />
     <EducationForm :education="education" />
+    <PersonForm :person="person" />
   </PageLayout>
 </template>
 
@@ -16,6 +17,7 @@ import { helpModal } from "@/mixins/modals";
 import PersonCard from '@/components/cards/PersonCard.vue';
 import PhotoPreview from '../ui/PhotoPreview.vue';
 import EducationForm from '../forms/EducationForm.vue';
+import PersonForm from '../forms/PersonForm.vue';
 
 
 
@@ -26,23 +28,21 @@ export default {
     PageLayout,
     PhotoPreview,
     PersonCard,
-    EducationForm
+    EducationForm,
+    PersonForm
     
   },
   data() {
     return {
       person: {
-        id: 'gi6437tf63i4ftfo43f',
-        secondName: 'Иванов',
+        lastName: 'Иванов',
         firstName: 'Иван',
-        patronymic: 'Иванович',
-        birth_date: '01.01.2000',
-        die_date: '',
-        gender: 'male',
-        children: ['y3g46yf34fy636b','3345fg35yg34f3f46'],
-        biography: 'Родился в ...',
-        activity: 'Бухгалтер',
-        photo: '/pictures/ivan.jpg'
+        middleName: 'Иванович',
+        gender: 'Мужской',
+        birthDate: '',
+        deathDate: '',
+        occupation: 'Студент',
+        bio: '',
       },
       education: {
         type: 'Бакалавриат',
