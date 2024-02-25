a8070af6ef20c8e546c6be77d46782e5f2cca0e8
Santiago-nve <Santiagonvebibang3@gmail.com>
2024-02-25 17:43:15 +0300

    Solution

--- vue/src/components/ui/SimpleButton.vue
+++ vue/src/components/ui/SimpleButton.vue
@@ -18,35 +18,34 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .simple-button {
-  padding: 15px 20px; 
-  font-size: 16px; 
+  padding: 15px 20px;
+  font-size: 16px;
   border: none;
-  border-radius: 8px; 
+  border-radius: 8px;
   cursor: pointer;
 
-  
-  font-family: "Arial", sans-serif; 
+  font-family: "Arial", sans-serif;
 }
 
 .button-info {
-  background-color: #ccc; 
+  background-color: #ccc;
   color: #000;
 }
 
 .button-warning {
-  background-color: #ff0; 
+  background-color: #ff0;
   color: #000;
 }
 
 .button-danger {
-  background-color: #f00; 
+  background-color: #f00;
   color: #fff;
 }
 
 .button-primary {
-  background-color: #5bc0de; 
+  background-color: #5bc0de;
   color: #fff;
 }
 </style>
