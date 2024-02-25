1cdf38a51f0398c5f1ca5cf1e40d44e92960a8b8
Santiago-nve <Santiagonvebibang3@gmail.com>
2024-02-25 11:34:21 +0300

    Solution

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -1,48 +1,101 @@
 <template>
-  <header>
-    <div class="wrapper">
-      <NavigationPanel />
-    </div>
-  </header>
+  <div>
+    <RouterView />
+    <ModalContainer />
+    <header>
+      <div class="wrapper">
+        <NavigationPanel />
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
-import SimpleButton from "./components/ui/SimpleButton.vue";
+import ModalContainer from "@/components/parts/ModalContainer";
 import NavigationPanel from "./components/ui/NavigationPanel.vue";
+import SimpleButton from "./components/ui/SimpleButton.vue";
 
 export default {
   components: {
-    SimpleButton,
+    ModalContainer,
     NavigationPanel,
+    SimpleButton,
   },
 };
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style lang="less">
+@import url("https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap");
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: @cBaseTwo;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+a {
+  text-decoration: none;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+section {
+  background-color: @cBaseOne;
+  margin-bottom: 20px;
+  border-radius: 2px;
+  box-sizing: border-box;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: @ffOne;
+  color: @cBaseThree;
+  margin: 0;
+}
+
+h2 {
+  font-size: 32px;
+}
+
+.p-16 {
+  padding: 16px;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.rcms {
+  &-divider {
+    &-h {
+      width: 100%;
+      height: @sizeBorderDefault;
+      background-color: @cBaseTwo;
+    }
+
+    &-v {
+    }
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  &-loading {
+    position: relative;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    &::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 1;
+      background: rgba(100, 100, 100, 0.5);
+      cursor: wait;
+    }
   }
 }
 </style>
