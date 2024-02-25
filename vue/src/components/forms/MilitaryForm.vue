ca77f1e37648b3abe32ff43288eeb1b170f64044
Danila <danila.dvoretskov@yandex.ru>
2024-02-26 00:55:25 +0300

    military_commit_3

--- vue/src/components/forms/MilitaryForm.vue
+++ vue/src/components/forms/MilitaryForm.vue
@@ -1,49 +1,44 @@
 <template>
   <form class="military-form">
     <div class="military-form__container">
-      <elInput
+      <ElInput
         v-model="form.type" 
-        :disabled="military.type ? true : false"
         class="military-form__inp" 
         type="text" 
         placeholder="Тип" 
       />
     </div>
     <div class="military-form__container">
-        <elInput 
-          v-model="form.rank"
-          :disabled="military.rank ? true : false"
-          class="military-form__inp" 
-          type="text" 
-          placeholder="Звание, должность" 
-        />
+      <ElInput 
+        v-model="form.rank"
+        class="military-form__inp" 
+        type="text" 
+        placeholder="Звание, должность" 
+      />
     </div>
     <div class="military-form__container">
-        <elInput
-          v-model="form.date_start"
-          :disabled="military.date_start ? true : false" 
-          class="military-form__inp" 
-          type="text" 
-          placeholder="Дата начала службы" 
-        />
+      <ElInput
+        v-model="form.date_start"
+        class="military-form__inp" 
+        type="text" 
+        placeholder="Дата начала службы" 
+      />
     </div>
     <div class="military-form__container">
-        <elInput 
-          v-model="form.date_end"
-          :disabled="military.date_end ? true : false" 
-          class="military-form__inp" 
-          type="text" 
-          placeholder="Дата конца службы" 
-        />
+      <ElInput 
+        v-model="form.date_end"
+        class="military-form__inp" 
+        type="text" 
+        placeholder="Дата конца службы" 
+      />
     </div>
     <div class="military-form__description">
-        <elInput
-          v-model="form.description" 
-          :disabled="military.description ? true : false" 
-          class="military-form__inp" 
-          type="textarea"
-          placeholder="Описание" 
-        />
+      <ElInput
+        v-model="form.description" 
+        class="military-form__inp" 
+        type="textarea"
+        placeholder="Описание" 
+      />
     </div>
   </form>
 </template>
@@ -75,15 +70,15 @@ export default {
 </script>
 
 <style lang="less">
-.military-form__inp{
+.military-form__inp {
   border: 1px solid #8d8d8d;
   color: #b4b4b4;
   border-radius: 6px;
-  & .el-input__inner{
+  & .el-input__inner {
     font-weight: 600;
     font-size: 18px;
   }
-  & .el-textarea__inner{
+  & .el-textarea__inner {
     font-weight: 600;
     font-size: 18px;
   }
