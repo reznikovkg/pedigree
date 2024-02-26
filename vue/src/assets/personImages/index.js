233ff6e35e9990f03547b223eeb71985bc2c637b
KsAdiia <113584597+KsAdiia@users.noreply.github.com>
2024-02-26 17:54:10 +0300

    fix photos

---
+++ vue/src/assets/personImages/index.js
@@ -0,0 +1,7 @@
+import person1 from "./person1.jpg";
+import person2 from "./person2.jpg";
+import person3 from "./person3.jpg";
+import person4 from "./person4.jpg";
+import person5 from "./person5.jpg";
+
+export { person1, person2, person3, person4, person5 };
