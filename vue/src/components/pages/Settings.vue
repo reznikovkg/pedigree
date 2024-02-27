0713cacf275903f5cd91f8cce682f3fdc89b0fdf
Anastasia Kolesnikova <koolanastasia@mail.ru>
2024-02-27 19:21:21 +0300

    First commit

---
+++ vue/src/components/pages/Settings.vue
@@ -0,0 +1,46 @@
+<template>
+    <PageLayout>
+      <section class="p-16">
+        <h1>
+            Settings
+        </h1>
+        <div>
+            <elSwitch
+                v-model= "value"
+                activeColor= "green"
+                inactiveColor= "grey"
+                activeValue= "True"
+                inactiveValue= "False">
+            </elSwitch>
+            Скрытый режим       
+        </div>
+      </section>
+    </PageLayout>
+</template>
+
+<script>
+  import PageLayout from '../parts/PageLayout'
+  
+  export default {
+    name: 'SettingsPage',
+    components: {
+      PageLayout
+    },
+    data() {
+      return {
+        value: []
+      }
+    },
+    mounted() {
+        this.value = JSON.parse(localStorage.getItem("value")) || []
+    },
+    watch: {
+        value(newValue) {
+            localStorage.setItem("value", JSON.stringify(newValue));
+        }
+    }
+  }
+</script>
+  
+<style>
+</style>
