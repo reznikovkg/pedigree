cbfe2bba2e8a4ff74b26821e02f0227761e00637
MRV <sabudilovskiy@yandex.ru>
2024-02-27 08:38:57 +0000

    some fixes

--- vue/src/components/cards/PersonPreviewCard.vue
+++ vue/src/components/cards/PersonPreviewCard.vue
@@ -29,50 +29,43 @@ export default {
   background-color: white;
   border-radius: 24px;
   background-color: #f5f5f5;
-  box-shadow: 1.5px 7px 5px 1.5px #0000001a, -1.5px 7px 5px -1.5px #00000033;
+  box-shadow: 1.5px 7px 5px 1.5px rgba(0, 0, 0, 0.1), -1.5px 7px 5px -1.5px rgba(0, 0, 0, 0.2);
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1em;
+  padding: 20px;
   position: relative;
-  margin: 1em;
+  margin: 20px;
   width: 425px;
   height: 215px;
-  &__person-info {
-    &__name,
-    &__patronymic,
-    &__date,
-    &__person-id {
-      margin-left: 10px;
-      font-family: 'Inter', sans-serif;
-    }
-    &__name {
-      margin-top: 10px;
-      margin-bottom: 10px;
-      color: #000000;
-    }
-    &__date {
-      color: #757575;
-      font-weight: bold;
-      white-space: nowrap;
-    }
-    &__person-id {
-       margin-top: 34px;
-       margin-bottom: 20px;
-       font-weight: bold;
-       color: #757575;
-    }
+  padding-left: 25px;
+  &__person-info__name {
+    font-family: 'Inter', sans-serif;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    color: #000000;
+  }
+  &__person-info__date {
+    font-family: 'Inter', sans-serif;
+    color: #757575;
+    font-weight: bold;
+    white-space: nowrap;
+  }
+  &__person-info__person-id {
+    font-family: 'Inter', sans-serif;
+    margin-top: 34px;
+    margin-bottom: 20px;
+    font-weight: bold;
+    color: #757575;
   }
   &__status-indicator {
     width: 25px;
     height: 25px;
     border-radius: 24px;
-    margin-top: 14px;
-    margin-right: 15px;
     background-color: #adffb4;
     position: absolute;
-    top: 10px;
-    right: 10px;
+    top: 25px;
+    right: 25px;
   }
 }
 
