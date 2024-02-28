1bb657c7a0dad12ea38570a0405edb812a5ca563
Anastasia Kolesnikova <koolanastasia@mail.ru>
2024-02-28 11:41:58 +0300

    Third

--- vue/src/components/pages/Settings.vue
+++ vue/src/components/pages/Settings.vue
@@ -6,11 +6,11 @@
       </h1>
       <div>
         <ElSwitch
-          v-model= "value"
-          activeColor= "green"
-          inactiveColor= "grey"
-          activeValue= "True"
-          inactiveValue= "False"/>
+          v-model="value"
+          activeColor="green"
+          inactiveColor="grey"
+          activeValue="True"
+          inactiveValue="False"/>
           Скрытый режим       
       </div>
     </section>
@@ -18,27 +18,27 @@
 </template>
 
 <script>
-  import PageLayout from '../parts/PageLayout'
-  
-  export default {
-    name: 'SettingsPage',
-    components: {
-      PageLayout
-    },
-    data () {
-      return {
-        value: []
-      }
-    },
-    mounted () {
-      this.value = JSON.parse(localStorage.getItem("value")) || []
-    },
-    watch: {
-        value(newValue) {
-          localStorage.setItem("value", JSON.stringify(newValue));
-        }
+import PageLayout from '../parts/PageLayout'
+
+export default {
+  name: 'SettingsPage',
+  components: {
+    PageLayout
+  },
+  data () {
+    return {
+      value: "False"
     }
+  },
+  mounted () {
+    this.value = JSON.parse(localStorage.getItem("value")) || []
+  },
+  watch: {
+      value(newValue) {
+        localStorage.setItem("value", JSON.stringify(newValue));
+      }
   }
+}
 </script>
   
 <style>
