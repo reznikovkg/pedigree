c4a92218ad9cd1be3b6c4036bb8038ecab85f4b5
Makarov Nikita <makarov.nikita.o@yandex.ru>
2024-02-29 16:21:44 +0300

    Добавлены комментарии

--- vue/src/types/person.ts
+++ vue/src/types/person.ts
@@ -8,50 +8,50 @@ type Date = string;
 
 interface Person {
   id: PersonId;
-  secondName: string;
-  firstName: string;
-  patronymic: string;
-  birth_date: Date;
-  die_date: Date;
-  gender: Gender;
-  biography: string;
-  activity: string;
-  photo: string;
-  children: PersonId[];
-  educations: Education[]; 
-  weddings: Wedding[];
-  works: Work[]; 
-  militaryServices: MilitaryService[];
+  secondName: string;                  // Фамилия
+  firstName: string;                   // Имя
+  patronymic: string;                  // Отчество
+  birth_date: Date;                    // Дата рождения
+  die_date: Date;                      // Дата смерти
+  gender: Gender;                      // Пол
+  biography: string;                   // Биография
+  activity: string;                    // Род деятельности
+  photo: string;                       // Ссылка на фото
+  children: PersonId[];                // Массив id детей
+  educations: Education[];             // Образования
+  weddings: Wedding[];                 // Брачные союзы
+  works: Work[];                       // Работы
+  militaryServices: MilitaryService[]; // Служба
 }
 
 interface Education {
-  type: string;
-  level: string;
-  startDate: Date;
-  endDate: Date;
-  institutionName: string;
-  institutionCity: string;
+  type: string;            // Тип образования
+  level: string;           // Уровень образования
+  startDate: Date;         // Дата начала обучения
+  endDate: Date;           // Дата завершения обучения
+  institutionName: string; // Полное название образовательного учреждения
+  institutionCity: string; // Город
 }
 
 interface Wedding {
-  partnerId: PersonId;
-  date_start: Date;
-  date_end: Date;
+  partnerId: PersonId; // Ссылка на партнера
+  date_start: Date;    // Дата заключения договора
+  date_end: Date;      // Дата расторжения брака
 }
 
 interface Work {
-  countryCity: string;
-  organization: string;
-  dateStart: Date;
-  dataEnd: Date;
-  jobTitle: string;
-  description: string;
+  countryCity: string;  // Город
+  organization: string; // Полное название организации
+  dateStart: Date;      // Дата начала работы
+  dataEnd: Date;        // Дата увольнения
+  jobTitle: string;     // Должность
+  description: string;  // Описание
 }
 
 interface MilitaryService {
-  type: string;
-  rank: string;
-  date_start: Date;
-  data_end: Date;
-  description: string;
+  type: string;        // Тип службы
+  rank: string;        // Звание, должность
+  date_start: Date;    // Дата начала службы
+  data_end: Date;      // Дата завершения службы
+  description: string; // Описание
 }
