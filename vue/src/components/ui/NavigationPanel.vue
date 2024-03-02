b0e132b4f47c31249f52257cebdd40303d86936b
Santiago-nve <Santiagonvebibang3@gmail.com>
2024-03-03 00:35:32 +0300

    add NavigationPanel

--- vue/src/components/ui/NavigationPanel.vue
+++ vue/src/components/ui/NavigationPanel.vue
@@ -1,3 +1,40 @@
---Ypu have to write your code in this file. After you need to do commit. You need to name your commit
-
---You have to write code here, in this file. After that you need to do commit. You need to save file. You can see changes. You need to add name of commit
+<template>
+    <div class="navigation-panel">
+      <SimpleButton type="info">Настройки</SimpleButton>
+      <SimpleButton type="warning">В центр</SimpleButton>
+      <SimpleButton type="danger">Удалить</SimpleButton>
+      <SimpleButton type="primary">Добавить</SimpleButton>
+    </div>
+  </template>
+  
+  <script>
+  import SimpleButton from "./SimpleButton.vue";
+  
+  export default {
+    components: {
+      SimpleButton,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .navigation-panel {
+    display: flex;
+    justify-content: space-between;
+    font-family: "Montserrat", sans-serif;
+    padding: 10px;
+  }
+  
+  .navigation-panel .simple-button {
+    flex: 1;
+    width: 150px;
+    margin-right: 10px;
+    color: #000000c7;
+    font-weight: 800;
+  }
+  
+  .navigation-panel .simple-button:last-child {
+    margin-right: 0;
+  }
+  </style>
+  
