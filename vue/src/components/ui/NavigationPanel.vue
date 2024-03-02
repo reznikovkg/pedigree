3650047bce990ff7d359a75b773b00d2c6cded2f
Santiago-nve <Santiagonvebibang3@gmail.com>
2024-03-03 00:30:31 +0300

    Add comments

--- vue/src/components/ui/NavigationPanel.vue
+++ vue/src/components/ui/NavigationPanel.vue
@@ -1,1 +1,3 @@
 --Ypu have to write your code in this file. After you need to do commit. You need to name your commit
+
+--You have to write code here, in this file. After that you need to do commit. You need to save file. You can see changes. You need to add name of commit
