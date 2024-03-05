4d7179ec3938159fc339b806442a0fc92361f4c2
Vladislav Nich <vnichug202@yandex.ru>
2024-03-05 14:35:17 +0300

    First commit (add files to components and types)

---
+++ vue/src/components/EducationList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="education-list">
+      <h2>Образование</h2>
+      <EducationItem v-for="(education, index) in educations" :key="index" :education="education" />
+    </div>
+  </template>
+  
+  <script>
+  import EducationItem from './EducationItem.vue';
+  
+  export default {
+    name: 'EducationList',
+    components: {
+        EducationItem
+    },
+    props: {
+    educations: {
+        type: Array,
+        required: true
+      }
+    }
+  };
+  </script>
+  
