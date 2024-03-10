7fa519e21bc3c2b61a56a7c3d85b0a29079a0ee2
nazarevaa <L120702@mail.ru>
2024-03-10 21:53:03 +0600

    Added header and footer

---
+++ vue/src/components/ui/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <header class="header">
+      <nav class="navbar">
+        <ul class="nav-menu">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">На главную</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderComponent'
+  };
+  </script>
+  
+  <style scoped>
+  .header {
+    width: 100%;
+    height: 60px;
+    background-color: #f2f2f2;
+  }
+  
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    padding: 0 20px;
+  }
+  
+  .nav-menu {
+    list-style: none;
+    display: flex;
+    align-items: center;
+  }
+  
+  .nav-item {
+    margin-right: 10px;
+  }
+  
+  .nav-link {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+  }
+  </style>
