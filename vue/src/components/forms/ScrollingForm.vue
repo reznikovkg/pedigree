41de136991dcabe26ca6729e8ff125bf02e785d5
bokla <boklagov.egor@yandex.ru>
2024-03-17 12:36:37 +0300

    1_scroll_commit

--- vue/src/components/forms/ScrollingForm.vue
+++ vue/src/components/forms/ScrollingForm.vue
@@ -1,57 +1,55 @@
 <template>
-    <div class="scrolling-menu">
-      <ul>
-        <li v-for="(section, index) in sections" :key="index">
-          <button @click="scrollToSection(section.id)">{{ section.title }}</button>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ScrollingForm',
-    props: {
-      sections: {
-        type: Array,
-        required: true
-      }
-    },
-    methods: {
-      scrollToSection (sectionId) {
-        const element = document.getElementById(sectionId)
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth' })
-        }
+  <div class="scrolling-menu">
+    <ul>
+      <li v-for="(section, index) in sections" :key="index">
+        <button @click="scrollToSection(section.id)">{{ section.title }}</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollingForm',
+  props: {
+    sections: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    scrollToSection (sectionId) {
+      const element = document.getElementById(sectionId)
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' })
       }
     }
   }
-  </script>
-  
-  <style>
-  .scrolling-menu {
-    position: fixed;
-    top: 50px;
-    left: 10px; 
-    z-index: 1000; 
-  }
-  
-  .scrolling-menu ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  
-  .scrolling-menu li {
-    margin-bottom: 5px;
-  }
-  
-  .scrolling-menu button {
-    background-color: #45475F00;
-    border: none;
-    padding: 8px 12px;
-    cursor: pointer;
-    font-weight: 600;
-    font-size:16px;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped lang="less">
+.scrolling-menu {
+  position: fixed;
+  top: 50px;
+  left: 10px; 
+}
+
+.scrolling-menu ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.scrolling-menu li {
+  margin-bottom: 5px;
+}
+
+.scrolling-menu button {
+  background-color: #45475F00;
+  border: none;
+  padding: 8px 12px;
+  cursor: pointer;
+  font-weight: 600;
+  font-size:16px;
+}
+</style>
