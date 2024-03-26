222dad81bef88d0a739650cb37b3d00e8ad499a2
vladenich1 <vnichug202@yandex.ru>
2024-03-26 09:30:47 +0300

    modified:   vue/src/router/routesPlugin.js

--- vue/src/router/routesPlugin.js
+++ vue/src/router/routesPlugin.js
@@ -4,13 +4,6 @@ import { RouteNames } from './routes'
 const RoutesPlugin = {
   install(Vue) {
     Vue.prototype.$routes = RouteNames
-    Vue.mixin({
-      data() {
-        return {
-          routes: RouteNames
-        }
-      }
-    })
   }
 }
 
